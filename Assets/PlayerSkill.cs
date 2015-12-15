f30ac54b7eebcae7e75278574b4b38e3a4f69b19
nepia12 <0fm38r05348532h@ezweb.ne.jp>
2015-12-15 16:35:10 +0900

    同じものだと思います

--- Assets/PlayerSkill.cs
+++ Assets/PlayerSkill.cs
@@ -1,9 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class PlayerSkill : MonoBehaviour {
-	public GameObject normalhitpar;
+	//skill01
+	public GameObject Rangehitpar;
 	public GameObject skillPoint;
+	private float skillrecast01;
+	public float Maxskillrecast01;
+	public GameObject skillbutton;
+	//slill02
+	public GameObject FRangehitpar;
+	public GameObject FskillPoint;
+	private float skillrecast02;
+	public float Maxskillrecast02;
+	public GameObject Frontskillbutton;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -11,9 +23,24 @@ public class PlayerSkill : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		skillrecast01-=1*Time.deltaTime;
+		skillrecast02-=1*Time.deltaTime;
+		if (skillrecast01 < 0) {
+			skillbutton.GetComponent<Button>().enabled=true;
+		}
+		if (skillrecast02 < 0) {
+			Frontskillbutton.GetComponent<Button>().enabled=true;
+		}
 	
 	}
 	public void Skill01(){
-		Instantiate(normalhitpar,skillPoint.transform.position,normalhitpar.transform.rotation);
+		Instantiate(Rangehitpar,skillPoint.transform.position,Rangehitpar.transform.rotation);
+		skillrecast01 = Maxskillrecast01;
+		skillbutton.GetComponent<Button>().enabled=false;
+	}
+	public void Skill02(){
+		Instantiate(FRangehitpar,FskillPoint.transform.position,FRangehitpar.transform.rotation);
+		skillrecast02 = Maxskillrecast02;
+		Frontskillbutton.GetComponent<Button>().enabled=false;
 	}
 }
--- Assets/RangeDamage.cs
+++ Assets/RangeDamage.cs
@@ -3,14 +3,16 @@ using System.Collections;
 
 public class RangeDamage : MonoBehaviour {
 	private GameObject enemyOb;
+	private GameObject enemyEOOb;
 	public GameObject player;
 	private float atk;
 	private float dmg;
+	public float downdmg;
 	// Use this for initialization
 	void Start () {
 		player = GameObject.Find ("Player");
 		atk=player.GetComponent<PlayerStatus>().Atk;
-		dmg = atk / 2;
+		dmg = atk / downdmg;
 	}
 	
 	// Update is called once per frame
@@ -24,5 +26,11 @@ public class RangeDamage : MonoBehaviour {
 			enemyOb.GetComponent<Enemy> ().life -=Mathf.Ceil(dmg);
 			Debug.Log("RANGEHIT");
 		}
+		if (col.gameObject.tag == "Enemy_EO") {
+			enemyEOOb=col.gameObject;
+			enemyEOOb.GetComponent<EnemyO> ().life -=Mathf.Ceil(dmg);
+			Debug.Log("RANGEHIT");
+		}
+
 	}
 }
