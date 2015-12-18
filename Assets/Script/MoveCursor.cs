6d21d69441d4252d3be95fffa9f05c5666140894
nepia12 <0fm38r05348532h@ezweb.ne.jp>
2015-12-18 16:33:15 +0900

    範囲移動　複数プレイヤースキル

---
+++ Assets/Script/MoveCursor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCursor : MonoBehaviour {
+	public GameObject prefab;
+	private GameObject cursor;
+	private Vector3 clickPosition;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			clickPosition=Input.mousePosition;
+			clickPosition.z=86f;
+			Instantiate(prefab,Camera.main.ScreenToWorldPoint(clickPosition),prefab.transform.rotation);
+
+		}
+	}
+}
