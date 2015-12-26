3574a2ab8ea1751e7726791bf4b24c0652cda7c0
nepia12 <0fm38r05348532h@ezweb.ne.jp>
2015-12-27 02:53:54 +0900

    Playerが体力40パーセント以下になったら攻撃力が1.1倍になるスキルの追加

--- Assets/Script/PlayerSkill.cs
+++ Assets/Script/PlayerSkill.cs
@@ -10,18 +10,21 @@ public class PlayerSkill : MonoBehaviour {
 		Heal,
 	}
 	public enumType TYPE;
-
+	public float par=1;
+	private float mag;
+	public float bufftimer;
+	public bool buffset=true;
 	// Use this for initialization
 	void Start () {
+		mag = GetComponent<PlayerStatus> ().MAXHP /par;
 		switch (TYPE) {
-		case enumType.AtkBuffer:
-			AtkBuffer();
-			break;
+	
 		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
 		switch (TYPE) {
 		case enumType.Bomb:
 			Bomb();
@@ -29,6 +32,15 @@ public class PlayerSkill : MonoBehaviour {
 		case enumType.Heal:
 			Heal();
 			break;
+		case enumType.AtkBuffer:
+			if(GetComponent<PlayerStatus>().HP<=mag&&buffset==true){
+			AtkBuffer();
+			}
+			break;
+		}
+		bufftimer-=1*Time.deltaTime;
+		if (bufftimer <= 0) {
+			GetComponent<PlayerStatus>().Atk=GetComponent<PlayerStatus>().MAXATK;
 		}
 	}
 	public void Bomb(){
@@ -37,11 +49,12 @@ public class PlayerSkill : MonoBehaviour {
 		}
 	}
 	public void AtkBuffer(){
-		GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
-		foreach(GameObject player in players) {
-			player.GetComponent<PlayerStatus>().Atk*=2;
-		}
-		Debug.Log ("ATK!!");
+		GameObject[] players = GameObject.FindGameObjectsWithTag ("Player");
+				foreach (GameObject player in players) {
+				player.GetComponent<PlayerStatus> ().Atk *= 2.1f;
+			player.GetComponent<PlayerSkill>().bufftimer=30;
+			buffset=false;
+				}
 	}
 	public void Heal(){
 		Debug.Log ("HEAL!!");
