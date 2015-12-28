faea5020e276a631ae3b705e42c1de96312ae2de
nepia12 <0fm38r05348532h@ezweb.ne.jp>
2015-12-29 04:33:47 +0900

    ゲージ使用スキルの実装 移動速度についてはエネミーの移動が不明なため自分の移動速度が75パーセント減少するようにしてあります

---
+++ Assets/SkillGageSC.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class SkillGageSC : MonoBehaviour {
+	public Image gage; 
+	public float SkillPoint;
+	public float MaxPoint=100;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		SkillPoint = Mathf.Clamp (SkillPoint,0,MaxPoint);
+		gage.fillAmount=SkillPoint/MaxPoint ;
+	}
+}
