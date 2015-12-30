00252a4fcb12dea63067d808f4842acfb757dac9
inusaku <bigwave_whale@yahoo.co.jp>
2015-12-30 14:22:21 +0900

    シーン移行の実装

---
+++ Assets/Script/selectManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class selectManager : MonoBehaviour {
+	private float timer;
+	private bool isEnd;
+	private bool isStart;
+	public string mainScene;
+
+	// Use this for initialization
+	void Start () {
+		timer = 0f;
+		isEnd = false;
+		isStart = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(isEnd == true || isStart == true){
+			timer += 0.9f * Time.deltaTime;
+		}
+		
+		if(timer > 1f && isEnd == true){
+			Application.LoadLevel("Title");
+		}
+		if(timer > 1f && isStart == true){
+			Application.LoadLevel("" + mainScene);
+		}
+	}
+	
+	public void end (){
+		if(isEnd == false && isStart == false){
+			isEnd = true;
+			GameObject.Find("ui_fead").GetComponent<Animator>().SetBool("isStart", true);
+		}
+	}
+	public void start (){
+		if(isEnd == false && isStart == false){
+			isStart = true;
+			GameObject.Find("ui_fead").GetComponent<Animator>().SetBool("isStart", true);
+		}
+	}
+}
