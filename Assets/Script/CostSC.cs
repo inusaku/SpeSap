44e8aaf40c39de54465c3886c94ce55fa13ebb0b
nepia12 <0fm38r05348532h@ezweb.ne.jp>
2015-12-31 03:42:14 +0900

    拠点Player生成　拠点生成Playerメニュー　カーソル改善　コスト

---
+++ Assets/Script/CostSC.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class CostSC : MonoBehaviour {
+	public Text costtext;
+	public int cost=0;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		costtext.text = "Cost:" + cost.ToString ();
+	}
+}
--- Assets/Script/MoveCursor.cs
+++ Assets/Script/MoveCursor.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class MoveCursor : MonoBehaviour {
 	public GameObject prefab;
 	private GameObject cursor;
+	public GameObject menu;
 	private Vector3 clickPosition;
 	// Use this for initialization
 	void Start () {
@@ -12,11 +13,20 @@ public class MoveCursor : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetMouseButtonDown (0)) {
-			clickPosition=Input.mousePosition;
-			clickPosition.z=64f;
-			Instantiate(prefab,Camera.main.ScreenToWorldPoint(clickPosition),prefab.transform.rotation);
 
+
+		if (Input.GetMouseButtonDown (0)) {
+			//clickPosition=Input.mousePosition;
+			//clickPosition.z=64f;
+			RaycastHit hit = new RaycastHit ();
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			if(Physics.Raycast(ray,out hit)){
+			Instantiate(prefab,hit.point+new Vector3(0,1,0),prefab.transform.rotation);
+				if(hit.collider.gameObject.tag=="kyoten"){
+					menu.SetActive(true);
+					Debug.Log("kyotenn");
+				}
+			}
 		}
 	}
 }
