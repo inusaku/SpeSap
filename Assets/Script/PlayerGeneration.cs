ac2f978d16b26c56f1ad15bc1fb590fc79a16d65
nepia12 <0fm38r05348532h@ezweb.ne.jp>
2016-01-14 00:18:15 +0900

    スピード型の追加

--- Assets/Script/PlayerGeneration.cs
+++ Assets/Script/PlayerGeneration.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class PlayerGeneration : MonoBehaviour {
 	public GameObject player01;
 	public GameObject player02;
+	public GameObject player03;
 	public GameObject PlayerHP;
 	public GameObject menu;
 	public GameObject cost;
@@ -13,12 +14,8 @@ public class PlayerGeneration : MonoBehaviour {
 	private float y;
 	// Use this for initialization
 	void Start () {
-<<<<<<< HEAD
-
-=======
 //		cost = GameObject.Find ("cost_text");
 //		menu = GameObject.Find ("PlayerMenu");
->>>>>>> origin/CHIBA
 	}
 	
 	// Update is called once per frame
@@ -31,14 +28,7 @@ public class PlayerGeneration : MonoBehaviour {
 		if (cost.GetComponent<CostSC> ().cost>=player01.gameObject.GetComponent<PlayerStatus>().cost) {
 			Vector3 pos = new Vector3 (x, kyotenpos.y, y);
 			Instantiate (player01, pos, player01.transform.rotation);
-<<<<<<< HEAD
 			Instantiate (PlayerHP);
-			_child = player01.transform.FindChild ("par3").gameObject;
-			_child.GetComponent<ParticleSystem>().startColor=Color.red;
-=======
-			Instantiate (PlayerHP, new Vector3(10000,0,0), Quaternion.identity);
-
->>>>>>> origin/CHIBA
 			cost.GetComponent<CostSC> ().cost -= player01.gameObject.GetComponent<PlayerStatus>().cost;
 		}
 
@@ -48,17 +38,20 @@ public class PlayerGeneration : MonoBehaviour {
 			Vector3 pos = new Vector3 (x, kyotenpos.y, y);
 			
 			Instantiate (player02, pos, player02.transform.rotation);
-<<<<<<< HEAD
 			Instantiate (PlayerHP);
-			_child = player02.transform.FindChild ("par3").gameObject;
-			_child.GetComponent<ParticleSystem>().startColor=Color.cyan;
-=======
-			Instantiate (PlayerHP, new Vector3(10000,0,0), Quaternion.identity);
-
->>>>>>> origin/CHIBA
 			cost.GetComponent<CostSC> ().cost -= player02.gameObject.GetComponent<PlayerStatus>().cost;
 		}
 
+	}
+	public void Player03(){
+		if (cost.GetComponent<CostSC> ().cost>=player03.gameObject.GetComponent<PlayerStatus>().cost) {
+			Vector3 pos = new Vector3 (x, kyotenpos.y, y);
+			
+			Instantiate (player03, pos, player02.transform.rotation);
+			Instantiate (PlayerHP);
+			cost.GetComponent<CostSC> ().cost -= player03.gameObject.GetComponent<PlayerStatus>().cost;
+		}
+		
 	}
 	public void close()
 	{
